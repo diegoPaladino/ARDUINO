b5d82c263bf259d33da659a07bb0c89a7b3fa8b8
DIEGO FARIAS GOMES <diegopaladinofoto@gmail.com>
2023-12-29 06:03:40 -0300

    AquaRegulaPura
    ### Nome do Projeto:
    **AquaRegula Pura** - Sistema de Controle de Dispensação de Água Inteligente.
    
    ### Lista de Materiais:
    1. **Arduino Nano:** O cérebro do projeto, controla a lógica e o processamento dos sinais dos sensores.
    2. **Sensor de Nível de Água:** Para detectar o nível de água no filtro de barro e enviar um sinal ao Arduino.
    3. **Válvula Solenoide:** Controla o fluxo de água; fechando quando o nível desejado for atingido.
    4. **Resistores:** Para proteger e estabilizar os circuitos.
    5. **Fios de Conexão:** Para interligar os componentes.
    6. **Fonte de Alimentação:** Para fornecer energia ao Arduino e à válvula solenoide.
    7. **Protoboard ou Placa de Circuito Impresso (PCB):** Para montar o circuito de forma organizada.
    8. **Fitas ou Suportes de Fixação:** Para fixar os componentes no filtro de barro.
    
    ### Passo a Passo de Montagem:
    1. **Montagem do Circuito:**
       - Conecte o Arduino Nano à protoboard.
       - Ligue o sensor de nível de água ao Arduino usando os fios e resistores conforme necessário.
       - Conecte a válvula solenoide ao Arduino, garantindo o uso de resistores para proteção.
       
    2. **Instalação no Filtro de Barro:**
       - Posicione o sensor de nível de água no ponto desejado dentro do filtro para detectar o nível máximo de água.
       - Instale a válvula solenoide na entrada de água do filtro de barro.
       - Fixe o Arduino e a protoboard em um local seguro e seco próximo ao filtro.
    
    3. **Alimentação:**
       - Conecte a fonte de alimentação ao Arduino e à válvula solenoide.
    
    ### Código para Arduino:
    ```cpp
    // Definições dos pinos
    const int pinSensorNivel = 2;  // Pino conectado ao sensor de nível de água
    const int pinValvula = 3;      // Pino conectado à válvula solenoide
    
    void setup() {
      pinMode(pinSensorNivel, INPUT); // Define o pino do sensor como entrada
      pinMode(pinValvula, OUTPUT);    // Define o pino da válvula como saída
      digitalWrite(pinValvula, HIGH); // Inicia com a válvula fechada
    }
    
    void loop() {
      int nivelAgua = digitalRead(pinSensorNivel); // Lê o estado do sensor de nível de água
    
      // Se o sensor detectar água (supondo que LOW significa que a água atingiu o sensor)
      if (nivelAgua == LOW) {
        digitalWrite(pinValvula, HIGH); // Fecha a válvula
      } else {
        digitalWrite(pinValvula, LOW); // Abre a válvula
      }
      
      // Um pequeno atraso para evitar leituras ruidosas
      delay(200);
    }
    ```
    
    ### Observações:
    - **Segurança:** Certifique-se de que todos os componentes elétricos estão devidamente isolados e protegidos da água.
    - **Testes:** Antes de instalar no filtro de barro, teste o sistema em um ambiente controlado para garantir seu funcionamento.
    - **Manutenção:** Verifique periodicamente as conexões e o funcionamento do sensor e da válvula.

---
+++ AquaRegula Pura/AquaRegula_Pura.ino
@@ -0,0 +1,23 @@
+// Definições dos pinos
+const int pinSensorNivel = 2;  // Pino conectado ao sensor de nível de água
+const int pinValvula = 3;      // Pino conectado à válvula solenoide
+
+void setup() {
+  pinMode(pinSensorNivel, INPUT); // Define o pino do sensor como entrada
+  pinMode(pinValvula, OUTPUT);    // Define o pino da válvula como saída
+  digitalWrite(pinValvula, HIGH); // Inicia com a válvula fechada
+}
+
+void loop() {
+  int nivelAgua = digitalRead(pinSensorNivel); // Lê o estado do sensor de nível de água
+
+  // Se o sensor detectar água (supondo que LOW significa que a água atingiu o sensor)
+  if (nivelAgua == LOW) {
+    digitalWrite(pinValvula, HIGH); // Fecha a válvula
+  } else {
+    digitalWrite(pinValvula, LOW); // Abre a válvula
+  }
+  
+  // Um pequeno atraso para evitar leituras ruidosas
+  delay(200);
+}
